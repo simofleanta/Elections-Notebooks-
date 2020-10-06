e3ad343d98f07f74d23f6aca5b89e1be9654ab16
simofleanta <simo.fleanta@gmail.com>
2020-10-06 11:17:36 +0200

    save

---
+++ countries.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import seaborn as sns 
+from pandas import DataFrame
+import matplotlib.pyplot as plt
+ 
+
+
+#open the file
+c=pd.read_csv('countries.csv')
+print(c)
+df=DataFrame(c)
+
+sns.violinplot(x=df["year"], y=df["population"], palette="Blues")
+plt.show()
+viz2 = sns.distplot(df["population"])
+print(viz2)
+
+
+#take one country 
+
+DE=df[df.country=='Germany']
+print(DE)
+
+DE_implot = sns.lmplot(data=DE, x='year', y='population',
+                 fit_reg=False)
+print(DE_implot)
+DE_hist = sns.distplot(DE["population"])
+print(DE_hist)
+
+
+
+
+
+
