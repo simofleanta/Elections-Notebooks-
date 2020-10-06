a8c09c0b53850d8f35e3d6d031e88e8299402c12
simofleanta <simo.fleanta@gmail.com>
2020-10-06 10:59:49 +0200

    save it

--- age_groups_cities.py
+++ age_groups_cities.py
@@ -11,12 +11,40 @@ print(age)
 df=DataFrame(age)
 
 
+#perform violin plots based on city
 sns.violinplot(x=df["City"], y=df["30_34"], palette="Blues")
 plt.show()
 sns.violinplot(x=df["City"], y=df["20_14"], palette="Blues")
 plt.show()
 sns.violinplot(x=df["City"], y=df["15_19"], palette="Blues")
 plt.show()
+
+#extract a city 
+Bucharest=df[df.City=='Bucuresti']
+print(Bucharest)
+
+#and perform charts on the specific city
+Bucharest_vis= sns.lmplot(data=Bucharest, x='15_19', y='20_14',
+                 fit_reg=False)
+print(Bucharest_vis)
+
+B_vis = sns.pairplot(Bucharest, vars=['Year','20_14'])
+print(B_vis)
+
+Bucx= sns.boxplot(data=Bucharest, x="Year", y="30_34")
+print(Bucx)
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
